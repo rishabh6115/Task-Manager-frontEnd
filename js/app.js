93bce25d664eca0ed555025785fcfe368bf50753
Rishabh Ranjan <77900932+rishabh6115@users.noreply.github.com>
2021-10-17 01:41:57 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,60 @@
+const input = document.querySelector('input')
+const btn = document.querySelector('button')
+const section = document.querySelector('.section')
+
+
+btn.addEventListener('click', () => {
+    const getInput = input.value;
+    addNewTask(getInput)
+
+})
+input.addEventListener('keydown', (e) => {
+    if (e.code == 'Enter' || e.code == 'NumpadEnter') {
+        const getInput = input.value;
+        addNewTask(getInput)
+    }
+})
+
+function addNewTask(getInput) {
+    const newDiv = document.createElement('div')
+
+    newDiv.classList.add('items')
+    const check = document.createElement('i')
+        // check.classList.add('far', "fa-check-circle", "icon0")
+        // newDiv.append(check)
+    section.append(newDiv)
+
+    const newSpan = document.createElement('div')
+    newSpan.classList.add('span')
+
+
+    newSpan.textContent = getInput
+
+    newDiv.append(newSpan)
+
+    const newIcons = document.createElement('div')
+    newIcons.classList.add('icons')
+    newSpan.append(newIcons)
+
+    const firstIcon = document.createElement('i')
+    firstIcon.classList.add('far', 'fa-trash-alt', 'icon1')
+    const SecondIcon = document.createElement('i')
+    SecondIcon.classList.add('far', 'fa-edit', 'icon2')
+    newIcons.append(firstIcon)
+    newIcons.append(SecondIcon)
+    input.value = '';
+    const del = document.querySelectorAll('.icon1')
+    for (let d of del) {
+        d.addEventListener('click', () => {
+            d.parentElement.parentElement.parentElement.remove()
+
+        })
+    }
+    const edit = document.querySelectorAll('.icon2')
+    for (let e of edit) {
+        e.addEventListener('click', () => {
+            e.parentElement.parentElement.parentElement.classList.toggle('tg')
+        })
+    }
+
+}
